b20ce12054734e14a7f3238d93770a6da87591a0
Chatterjee, Aniruddho Swapan <caswapan@iu.edu>
2023-12-10 16:27:56 -0500

    All functionalities added

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import json
-
 from flask import Flask,render_template,request,flash,session,g,url_for,redirect
 import requests
+from google.cloud import storage
+import os
 app = Flask(__name__,template_folder='templates')
 
 app.config['SECRET_KEY'] = 'secret-key-goes-here'
@@ -11,6 +12,23 @@ def index():
 
     return render_template('index.html')
 
+@app.route('/upload',methods=['GET','POST'])
+def upload():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            return redirect(request.url)
+
+        file = request.files['file']
+        if file.filename == '':
+            return redirect(request.url)
+        print(file.filename)
+        os.environ['GOOGLE_APPLICATION_CREDENTIALS']  = 'aniruddho-chatterjee-fall2023-f967fa2ed4a5.json'
+        client = storage.Client(project='aniruddho-chatterjee-fall2023')
+        bucket = client.get_bucket('books-dataset')
+        blob = bucket.blob(file.filename)
+        blob.upload_from_file(file,content_type='text/plain')
+        flash('File uploaded successfully!', 'success')
+    return render_template('upload.html')
 @app.route('/search/<string:inputText>', methods=['GET','POST'])
 def search(inputText):
     params = {"keyword":inputText}
