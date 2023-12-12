b9d83d07bf226c3c62b702e6e48b7be6ea03a644
Chatterjee, Aniruddho Swapan <caswapan@iu.edu>
2023-12-12 03:41:20 -0500

    Added Multiple Files Compatibility

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask,render_template,request,flash,session,g,url_for,redirect
 import requests
 from google.cloud import storage
 import os
-import gunicorn
+from flask_googlestorage import GoogleStorage, Bucket
 app = Flask(__name__,template_folder='templates')
 
 app.config['SECRET_KEY'] = 'secret-key-goes-here'
@@ -16,19 +16,14 @@ def index():
 @app.route('/upload',methods=['GET','POST'])
 def upload():
     if request.method == 'POST':
-        if 'file' not in request.files:
-            return redirect(request.url)
-
-        file = request.files['file']
-        if file.filename == '':
-            return redirect(request.url)
-        print(file.filename)
-        os.environ['GOOGLE_APPLICATION_CREDENTIALS']  = 'aniruddho-chatterjee-fall2023-f967fa2ed4a5.json'
+        files = request.files.getlist("files")
+        os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'aniruddho-chatterjee-fall2023-acb87efd5ca0.json'
         client = storage.Client(project='aniruddho-chatterjee-fall2023')
         bucket = client.get_bucket('books-dataset')
-        blob = bucket.blob(file.filename)
-        blob.upload_from_file(file,content_type='text/plain')
-        flash('File uploaded successfully!', 'success')
+        for file in files:
+            blob = bucket.blob(file.filename)
+            blob.upload_from_file(file,content_type='text/plain')
+            flash(f'File {file.filename} uploaded successfully!', 'success')
     return render_template('upload.html')
 @app.route('/search/<string:inputText>', methods=['GET','POST'])
 def search(inputText):
